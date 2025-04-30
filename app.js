235e5f09d863fc1eaf46b1ffd32564730d7b1a18
sandrine-bd <sandrine.barrucand@gmail.com>
2025-04-30 09:51:22 +0200

    feat(feed) : nom prénom

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
-const feedContainer = document.getElementByClass('feed');
-const refreshBtn = document.getElementByClass('refresh');
-
 // Fonction pour charger les articles
 async function showUser() {
+  try { 
     const response = await fetch("https://randomuser.me/api/0.8/?results=10");
-    const users = await response.json();
+    const data = await response.json();
+    const users = data.results;
     console.log(users);
+
+  // Affiche les utilisateurs dans le DOM
+  const container = document.getElementsByClassName('feed')[0] // ajoute le contenu de container au DOM
+  container.innerHTML = ''; // nettoyage du contenu précédent
+
+  users.forEach(user => {
+  const div = document.createElement('div') // crée un nouvel élément div
+  div.textContent = `${user.user.name.first} ${user.user.name.last}` // lui donne du contenu
+  container.appendChild(div) // ajoute un nœud (enfant div) à la fin de la liste des enfants d'un nœud parent spécifié (container)
+  })
+} catch (error) {
+    console.error("Erreur lors du chargement des utilisateurs :", error);
   }
-  
-showUser()
+}
 
-// Fonction pour créer un bloc d'article
-function createPostElement(post) {
-    const article = document.createElement('article');
-    article.innerHTML = `
-      <h2>${post.title}</h2>
-      <p>${post.body}</p>
-    `;
-    return article;
-  }
+// Appel lors du chargement de la page
+document.addEventListener('DOMContentLoaded', showUser)
 
-
-  // Charger au démarrage
-window.addEventListener('DOMContentLoaded', loadFeed);
-
-// Recharger au clic
-refreshBtn.addEventListener('click', loadFeed);
-
-createPostElement(post)
+// Appel lors du clic sur le bouton d'actualisation
+document.getElementById('refresh-btn').addEventListener('click', showUser)
